a1b5115a483159b86556c716ac71490298338020
Lelouch <phuctinh2001@gmail.com>
2025-04-23 22:46:19 +0700

    update code

--- src/hivespace/constants/HiveSpaceConstantFE.groovy
+++ src/hivespace/constants/HiveSpaceConstantFE.groovy
@@ -7,7 +7,7 @@ public class HiveSpaceConstantFE {
     static HiveSpaceProject project = new HiveSpaceProject(
         name: 'HiveSpace frontend',
         gitRepo: 'https://github.com/HiveSpaceTeam/hivespace.frontend',
-        branch: 'master',
+        branch: 'production.g1',
         credentialsId: 'dockerhub-credentials',
         apps: [
             new HiveSpaceApp(name: 'frontend', dockerImage: 'dblmint/hivespace-frontend'),
