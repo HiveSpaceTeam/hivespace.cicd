e510f99057c1ffec985d6080b4e13e8af6fb94cf
Lelouch <phuctinh2001@gmail.com>
2025-05-14 23:03:02 +0700

    update

--- vars/deployStaticWebApp.groovy
+++ vars/deployStaticWebApp.groovy
@@ -2,17 +2,12 @@ import hivespace.entity.*
 
 def call() {
     stage('Install node dependencies') {
-        steps {
             sh 'npm install'
-        }
     }
     stage('Build Apps') {
-        steps {
             sh 'npm run build'
-        }
     }
     stage('Deploy to Azure Static Web Apps') {
-        steps {
             script {
                 def outputLocation = 'dist'
                 echo "🚀 Deploying to Azure Static Web Apps from ${outputLocation}"
@@ -20,6 +15,5 @@ def call() {
                 swa deploy ${outputLocation} --deployment-token $AZURE_STATIC_WEB_APPS_API_TOKEN  --env production
             """
             }
-        }
     }
 }
