2218750d498f62e9fd8244efcb79fbc2c8a980e5
Lelouch <phuctinh2001@gmail.com>
2025-05-16 21:47:19 +0700

    udpate

--- vars/buildApps.groovy
+++ vars/buildApps.groovy
@@ -1,12 +1,9 @@
 import hivespace.entity.*
 
 def call(HiveSpaceProject project) {
-    stage('BuildApps') {
-        echo 'build apps'
-        project.apps.each { app ->
+    echo 'build apps'
+    project.apps.each { app ->
             // app.build()
             echo "Building app: ${app.name}"
-        }
     }
-   
 }
