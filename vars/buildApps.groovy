090e41bbb7d3e269a22c98f1f7f0a9e074cc1194
Lelouch <phuctinh2001@gmail.com>
2025-05-16 21:46:13 +0700

    update

--- vars/buildApps.groovy
+++ vars/buildApps.groovy
@@ -1,14 +1,12 @@
 import hivespace.entity.*
 
 def call(HiveSpaceProject project) {
-    // stage('BuildApps') {
-    //     echo 'build apps'
-    //     project.apps.each { app ->
-    //         // app.build()
-    //         echo "Building app: ${app.name}"
-    //     }
-    // }
-    stage('Install node dependencies') {
-        sh 'npm install'
+    stage('BuildApps') {
+        echo 'build apps'
+        project.apps.each { app ->
+            // app.build()
+            echo "Building app: ${app.name}"
+        }
     }
+   
 }
