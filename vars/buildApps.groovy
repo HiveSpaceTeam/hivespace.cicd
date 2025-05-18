80b115f4bea74f3e79f7503e7e8df9bf2aa63db5
Lelouch <phuctinh2001@gmail.com>
2025-05-18 22:37:45 +0700

    as

--- vars/buildApps.groovy
+++ vars/buildApps.groovy
@@ -18,16 +18,20 @@ def call(HiveSpaceProject project) {
 }
 
 void deployDotnetCore(HiveSpaceProject project, HiveSpaceApp app) {
-    withCredentials([usernamePassword(
+    stage('Build & Push All Apps') {
+                steps {
+            withCredentials([usernamePassword(
                             credentialsId: project.credentialsId,
                             usernameVariable: 'DOCKER_USERNAME',
                             passwordVariable: 'DOCKER_PASSWORD'
                         )]) {
-        sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
+                sh "echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin"
 
-            def imageTag = app.getFullImageTag(tag)
-            echo "🔧 Building image for ${app.name} at ${app.buildContext}"
-            sh "docker build -t ${imageTag} ${app.buildContext}"
-            sh "docker push ${imageTag}"
+                def imageTag = app.getFullImageTag(tag)
+                echo "🔧 Building image for ${app.name} at ${app.buildContext}"
+                sh "docker build -t ${imageTag} ${app.buildContext}"
+                sh "docker push ${imageTag}"
                         }
+        }
+    }
 }
